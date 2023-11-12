b435b4d0f848689456096d159bf766d28546e522
christinanikolova <christinanikolova@github.com>
2023-11-12 15:14:25 +0200

    add Bottom component

---
+++ client/src/components/shared/images/Bottom.vue
@@ -0,0 +1,67 @@
+<script>
+export default {
+  props: {
+    first: {
+      type: String,
+      required: true,
+    },
+    second: {
+      type: String,
+      required: true,
+    },
+    third: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="bottom-img-wrapper">
+    <img
+      class="bottom-img img img-shadow"
+      :src="'./../../../img/' + first"
+      alt="wedding_flowers"
+    />
+    <img
+      class="bottom-img img img-shadow"
+      :src="'./../../../img/' + second"
+      alt="wedding_table"
+    />
+    <img
+      class="bottom-img img img-shadow"
+      :src="'./../../../img/' + third"
+      alt="wedding_invitations"
+    />
+  </div>
+</template>
+
+<style scoped>
+.bottom-img-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  padding-bottom: 40px;
+  margin-bottom: 40px;
+}
+
+.bottom-img {
+  max-width: 30%;
+  max-height: 20%;
+}
+
+@media screen and (max-width: 800px) {
+  .bottom-img-wrapper {
+    flex-direction: column;
+    gap: 12px;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .bottom-img-wrapper {
+    display: none;
+  }
+}
+</style>
--- client/src/utils/constants/images.js
+++ client/src/utils/constants/images.js
@@ -3,3 +3,9 @@ export const jumbo = {
   BLOG: "/img/wedding-3218833_1920.jpg",
   USER: "/img/wedding-2584187_1920.jpg",
 };
+
+export const bottom = {
+  FIRST: "bunch-of-flowers-363169_1920.jpg",
+  SECOND: "wedding-1760024_1920.jpg",
+  THIRD: "wedding-905240_1920.jpg",
+};
