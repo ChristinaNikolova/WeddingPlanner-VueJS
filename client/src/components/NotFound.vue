a92b96c8c198abfe038cb83d4267f2226c9a0b6d
christinanikolova <christinanikolova@github.com>
2023-11-12 14:38:40 +0200

    add NotFound component

---
+++ client/src/components/NotFound.vue
@@ -0,0 +1,114 @@
+<script>
+// todo import Link import { Link } from 'react-router-dom';
+export default {};
+</script>
+
+<template>
+  <section class="not-found">
+    <h2 class="not-found-main-title">404</h2>
+    <h4 class="not-found-sub-title">Page not found</h4>
+    <p class="not-found-content">
+      Sorry, the page you are looking for doesn't exist
+    </p>
+    <!-- <Link to="/" class="btn">Go back to home</Link> -->
+  </section>
+</template>
+
+<style scoped>
+.not-found {
+  background-color: var(--clr-creme);
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  position: relative;
+  height: 100vh;
+}
+
+.not-found-main-title {
+  font-size: 400px;
+  margin-bottom: 10px;
+}
+
+.not-found-sub-title {
+  font-size: 80px;
+  margin-bottom: 50px;
+}
+
+.not-found-content {
+  font-size: 30px;
+  margin-bottom: 80px;
+}
+
+@media screen and (max-width: 1200px) {
+  .not-found {
+    margin-bottom: 50px;
+  }
+
+  .not-found-main-title {
+    font-size: 350px;
+  }
+
+  .not-found-sub-title {
+    font-size: 70px;
+  }
+
+  .not-found-content {
+    font-size: 26px;
+  }
+}
+
+@media screen and (max-width: 800px) {
+  .not-found {
+    text-align: center;
+    padding-right: 20px;
+    padding-left: 20px;
+  }
+
+  .not-found-main-title {
+    font-size: 300px;
+  }
+
+  .not-found-sub-title {
+    font-size: 60px;
+  }
+
+  .not-found-content {
+    font-size: 24px;
+  }
+}
+
+@media screen and (max-width: 700px) {
+  .not-found {
+    margin-bottom: 90px;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .not-found-main-title {
+    font-size: 200px;
+  }
+
+  .not-found-sub-title {
+    font-size: 50px;
+  }
+
+  .not-found-content {
+    font-size: 20px;
+  }
+}
+
+@media screen and (max-width: 400px) {
+  .not-found-main-title {
+    font-size: 100px;
+  }
+
+  .not-found-sub-title {
+    font-size: 30px;
+  }
+
+  .not-found-content {
+    font-size: 18px;
+  }
+}
+</style>
