f7baa9bda304ecbc98664b59855f05852d9e1835
christinanikolova <christinanikolova@github.com>
2023-11-19 08:49:51 +0200

    add Form button component

---
+++ client/src/components/shared/buttons/Form.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  props: {
+    formName: {
+      type: String,
+      required: true,
+      default: 'Save',
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ['onCancelFormHandler'],
+};
+</script>
+
+<template>
+  <div class="form-btns-wrapper">
+    <button :disabled="isDisabled" class="btn btn-center">
+      {{ formName }}
+    </button>
+    <button class="btn btn-center" type="button" @click="$emit('onCancelFormHandler')">
+      Cancel
+    </button>
+  </div>
+</template>
