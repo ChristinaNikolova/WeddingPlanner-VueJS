09a51fa2969a8cd40ccd14aae83d6c3d18cf9337
christinanikolova <christinanikolova@github.com>
2023-12-18 08:00:42 +0200

    extend commentViewModel

--- server/utils/mapper/comment.js
+++ server/utils/mapper/comment.js
@@ -1,9 +1,14 @@
+const { formatCreatedAt } = require("../parser");
+
 function commentViewModel(comment) {
   return {
     id: comment._id,
     content: comment.content,
     creatorId: comment.creator._id,
     creatorName: getFullName(comment.creator),
+    likesCount: comment.likes.length,
+    likes: comment.likes,
+    createdAt: formatCreatedAt(comment.createdAt),
   };
 }
 
